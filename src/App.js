9c4429e71347d67c487e0d5d231ac6022d762bb5
kate121296 <katyamacegora1212@gmail.com>
2019-08-25 09:51:12 +0300

    work with axios

--- src/App.js
+++ src/App.js
@@ -1,16 +1,11 @@
 import React, { useState } from 'react';
-import { getOr } from 'lodash/fp'
-
 import './app.css';
-import Tabs from "./components/Tabs/Tabs";
-import Menu from "./components/Menu/Menu";
-import Vkladka2 from "./components/Vkladka2/Vkladka2";
 
 function App() {
-
+    const [data, setData] = useState([]);
     return (
         <div className="root">
-
+            <h1>hello</h1>
         </div>
     );
 }
