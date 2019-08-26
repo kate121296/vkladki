2fc57fbd92abdb8266f20e11528eb8cf51bf6978
kate121296 <katyamacegora1212@gmail.com>
2019-08-26 21:25:08 +0300

    master

--- src/App.js
+++ src/App.js
@@ -1,42 +1,10 @@
-import React, { useState, useEffect } from 'react';
-import { getOr } from 'lodash/fp'
-import axios from "axios";
-
-import './app.css';
-import Input from "./components2/Input/Input";
-import Ul from "./components2/Ul/Ul";
-
+import React from 'react';
+import './App.css';
 
 function App() {
-    const [isDataLoaded , setIsDataLoaded] = useState(false);
-    const [value , setValue] = useState('');
-    const [data, setData] = useState([]);
-    useEffect(() => {
-        if( isDataLoaded===false) {
-            axios.get('https://jsonplaceholder.typicode.com/users')
-                .then(function (response) {
-                    setData(response.data)
-                    setIsDataLoaded(true)
-                })
-        }
-    }, [isDataLoaded, data]); // Only re-run the effect if data changes
-
-    const handleChange = e => {
-        const val = getOr('', ['target', 'value'], e);
-        setValue(val)
-    };
-    const filteredItems = data.filter(item =>
-        getOr('', ['name'], item)
-            .toLowerCase()
-            .includes(value.toLowerCase()),
-    );
     return (
         <div className="root">
-           <Input
-               value={value}
-               onChange={handleChange}
-           />
-          <Ul data={filteredItems}/>
+           hello katya :)
         </div>
     );
 }
